fb4ccdb7f731fb095a4cd966cd2f6b7a3f3ac324
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-09-27 18:41:32 +0500

    GET Method frontend

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+let API_URL = 'http://localhost:3000/api/data';
 
 function App() {
+  let [users,setUsers] = useState([]);
+  useEffect(() => {
+  fetchedData()
+  },[]);
+  let fetchedData = () => {
+    axios.get(API_URL)
+  .then(function (response) {
+    // handle success
+    setUsers(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  }
   return (
-    <div>App</div>
+    <>
+    <ul>
+      {users.map((item) => (
+        <li key={item.id}>{item.name} <span className='ml-5'> <button className='border-2'>Edit</button> <button className='border-2'>Delete</button></span></li>
+      ))}
+    </ul>
+    </>
   )
 }
 
