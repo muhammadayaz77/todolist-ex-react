ec922ada1be14c344e5e1df569dee1aa7a24dfb2
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-09-27 18:31:03 +0500

    GET Method backend

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,12 +1,15 @@
 import express from 'express'
-
+import cors from 'cors'
 let app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.get("/",(req,res) => {
-  res.json({message : 'Home'})
+let users = [
+  {id : 1,name : 'ali',email : 'ali@gmail.com',},
+  {id : 2,name : 'khan',email : 'khan@gmail.com',},
+]
+app.get("/api/data",(req,res) => {
+  res.json(users)
 })
 
 app.listen(3000,() => {
